dd2eb8a5038a0b5c47e64b5ddcf610cd12000a3d
Andrey Kuzmenko <ABEJIb93@gmail.com>
2025-11-13 11:21:38 +0300

    add privacy

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,53 @@
+
+
+<section class="wrapper style2 pt-5 pb-5" >
+    <div class="container">
+        <div class="header mb-4">
+            <h1 class="align-center">Privacy Policy</h1>
+        </div>
+
+        <div class="privacy-content">
+            <p>This Privacy Policy (hereinafter - the Policy) of the "Delovoy" service (hereinafter - the Service) applies to the information that RAFAN (hereinafter - the Organization) may obtain from the User's device during their use of the Service.</p>
+
+            <p>Use of the Service means the User's consent to this Policy and the described conditions for collecting and processing information from the User's device. If the User does not agree with the terms of the Policy, they should refrain from using the Organization's Application.</p>
+
+            <h3 class="mt-4 mb-3">1. Composition of information that may be obtained from the User's device when using the Service and the purposes of its collection (hereinafter - User Data):</h3>
+
+            <h4 class="mt-3 mb-2">1.1. Information about phone numbers from the device's address book</h4>
+            <p>(if the corresponding type of device supports this functionality).</p>
+            <p><strong>Purpose:</strong> phone numbers from the contact address book on the user's device are used in the Service to simplify the procedure of money transfers and payment for mobile operator services.</p>
+
+            <h4 class="mt-3 mb-2">1.2. Information about the user's device location</h4>
+            <p>(based on mobile operator network data, GPS signals and other data obtained through the Internet).</p>
+            <p><strong>Purpose:</strong> informing the client when using the Service about the location of the Organization's branches (offices and ATMs) available to the user depending on their location.</p>
+
+            <h4 class="mt-3 mb-2">1.3. Photo and video images</h4>
+            <p>(including in streaming mode), obtained using the device's camera (if the corresponding type of device supports this functionality).</p>
+            <p><strong>Purpose:</strong> obtaining and using photo images as part of services implemented in the Service. Including for creating and saving photos and videos in the user's profile in the Service, obtaining photos of payment documents, QR codes and barcodes. Media data may be needed for money transfers and payment for services in the Service.</p>
+
+            <h4 class="mt-3 mb-2">1.4. Information about the operating system version and device model</h4>
+            <p><strong>Purpose:</strong> analysis of possible errors in the Service's operation and improvement of the Service's performance.</p>
+            <p>For analysis purposes, the Organization may transfer information about the operating system and device model to third-party organizations in anonymized form.</p>
+
+            <h4 class="mt-3 mb-2">1.5. Information about IP address and user's connection point address</h4>
+            <p><strong>Purpose:</strong> increasing the security of financial transactions when using the Service.</p>
+
+            <h4 class="mt-3 mb-2">1.6. Additional tools</h4>
+            <p>Additionally, within the framework of this Policy, the Organization has the right to use additional software tools (including those of the Organization's partners) and cookies to collect and process anonymized statistical information about the user's use of the Service and services within the Service for the purpose of improving the Service. Yandex.Metrica analytics system is used to collect and process anonymized information about user actions in the Service.</p>
+
+            <h3 class="mt-5 mb-3">2. Conditions for processing User Information</h3>
+
+            <p><strong>2.1.</strong> In accordance with the Policy, the Organization processes only that information and only for those purposes that are defined in section 1.</p>
+
+            <p><strong>2.2.</strong> The Organization takes all measures within its power to protect User Information from unauthorized access by third parties, use, copying and distribution.</p>
+
+            <p><strong>2.3.</strong> For the purposes set out in this Policy, the Organization may engage partners with whom the Organization has entered into appropriate confidentiality agreements to process User Information. Transfer of anonymized data on the use of the Service to partners for the purpose of improving the Service is carried out based on agreements with partners.</p>
+
+            <p><strong>2.4.</strong> User information may be stored on the Organization's and its partners' resources during the term of contractual relations between the Organization and the user regarding the Service.</p>
+
+            <p><strong>2.5.</strong> User Information may be provided to government authorities in accordance with the requirements of applicable law.</p>
+        </div>
+    </div>
+</section>
+
+
