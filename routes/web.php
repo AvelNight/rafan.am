366afcfcf559697b8e8995fffa4c20ec5186176e
Andrey Kuzmenko <ABEJIb93@gmail.com>
2025-11-13 11:20:24 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([SetLocale::class])->group(function () {
     Route::get('/', [MainController::class, 'index']);
     Route::post('/', [MainController::class, 'store'])->name('sendMessage');
+    Route::get('/privacy', [MainController::class, 'privacy'])->name('privacy');
 });
